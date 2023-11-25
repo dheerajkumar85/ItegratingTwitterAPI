cf5818c7e53151ca1621ba24be01ff5fbc2c0ca0
dheeraj kumar <dheerajkumar@dheerajs-Air.domain.name>
2023-11-25 15:01:38 +0530

    Integrating Twiter API

---
+++ Integrate.twitter.apis/src/test/java/com/Integrate/twitter/apis/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Integrate.twitter.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
